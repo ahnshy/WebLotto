7f68012d40fe802f44860e61b4434a97bca02d43
ahnshy <ahnshy@naver.com>
2026-05-06 22:00:23 +0900

    <fix> : AutoPrint file has been modified.

--- app/print/lotto/AutoPrint.tsx
+++ app/print/lotto/AutoPrint.tsx
@@ -2,14 +2,14 @@
 
 import * as React from 'react';
 
-export default function AutoPrint() {
+export default function AutoPrint({ delayMs = 300 }: { delayMs?: number }) {
   React.useEffect(() => {
     const timer = window.setTimeout(() => {
       window.print();
-    }, 300);
+    }, delayMs);
 
     return () => window.clearTimeout(timer);
-  }, []);
+  }, [delayMs]);
 
   return null;
 }
